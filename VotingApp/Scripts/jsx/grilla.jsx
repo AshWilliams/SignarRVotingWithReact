7e1f9e96211a207cbe9b112e8ef9004e6a865422
Robert Rozas Navarro <robert.rozas.n@gmail.com>
2018-02-06 12:55:38 -0300

    Primer commit

---
+++ VotingApp/Scripts/jsx/grilla.jsx
@@ -0,0 +1,60 @@
+﻿var dataGrilla = [
+    {
+        Id: 1,
+        Nombre: "Robert",
+        Apellido: "Rozas"
+    },
+    {
+        Id: 2,
+        Nombre: "Juan",
+        Apellido: "Ramos"
+    },
+    {
+        Id: 3,
+        Nombre: "Ruben",
+        Apellido: "Azocar"
+    }
+];
+
+var GrillaApp = React.createClass({
+    getInitialState: function () {
+        return { data: dataGrilla };
+    },
+
+    componentDidMount: function () {
+        
+    },
+
+    onClickEditSelected(Id) {
+        alert("Editar item " + Id);
+    },
+    onClickDeleteSelected(Id) {
+        alert("Eliminar item " + Id);
+    },
+
+    render: function () {
+        return (
+            <table className="table table-hover table-bordered">
+                <thead>
+                    <tr>
+                        <th>Nombre</th>
+                        <th>Apellido</th>
+                        <th>Editar</th>
+                        <th>Eliminar</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {this.state.data.map((obj, index) => <tr>
+                        <td>{obj.Nombre}</td>
+                        <td>{obj.Apellido}</td>
+                        <td><p data-placement="top" data-toggle="tooltip" title="Editar"><button className="btn btn-primary btn-xs" data-title="Editar" onClick={() => this.onClickEditSelected(obj.Id)}><span className="glyphicon glyphicon-pencil"></span></button></p></td>
+                        <td><p data-placement="top" data-toggle="tooltip" title="Eliminar"><button className="btn btn-danger btn-xs" data-title="Eliminar" onClick={() => this.onClickDeleteSelected(obj.Id)}><span className="glyphicon glyphicon-trash"></span></button></p></td>
+                    </tr>)}
+                </tbody>
+            </table>
+        );
+    }
+
+});
+
+React.render(<GrillaApp />, document.getElementById('tableResult'));
---
+++ VotingApp/VotingHub.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Newtonsoft.Json;
+
+namespace VotingApp
+{
+    public class VotingHub : Hub
+    {
+
+        private static Dictionary<string, int> poll = new Dictionary<string, int>(){
+             {"Apples",10 },
+             {"Oranges",10},
+             {"Bananas",10},
+             {"Blueberries",10},
+             {"mangoes",10},
+        };
+
+        public static Dictionary<string, int> Poll { get => poll; set => poll = value; }
+
+        public void Send(string name)
+        {
+            Poll[name]++;
+            string data = JsonConvert.SerializeObject(Poll.Select(x => new { name = x.Key, count = x.Value }).ToList());
+
+
+            Clients.All.showLiveResult(data);
+        }
+    }
+
+}
